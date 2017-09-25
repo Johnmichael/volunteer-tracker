8881178b2ab5bc27afdec08d42360dc1316a9df3
John Michael <john.parsons85210@gmail.com>
2017-09-24 22:28:15 -0700

    pass first spec

---
+++ lib/project.rb
@@ -0,0 +1,11 @@
+class Project
+  attr_reader(:id, :title, :description)
+
+  def initialize (attributes)
+    @id = attributes[:id]
+    @title = attributes[:title]
+    @description = attributes[:description]
+  end
+
+
+end
